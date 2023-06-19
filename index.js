63324b78555121654da0c5fc5e15c2efa53e1336
Ayush757 <ayushhh.a757@gmail.com>
2023-06-19 18:47:32 +0530

    Hello

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const cron = require('node-cron');
-const app = express();
-
-app.use(express.json());
-
-app.post('/schedule-notification', (req, res) => {
-  const { date, message } = req.body;
-
-  const cronExpression = new Date(date).toISOString();
-
-  const scheduledTask = cron.schedule(cronExpression, () => {
-    console.log('Sending notification:', message);
-  });
-
-  res.status(200).json({ message: 'Notification scheduled successfully' });
-});
-
 const port = 3000;
+const app = express();
+const bodyParser = require('body-parser');
 
+const schedule=require("./routes/schedule")
+app.use(bodyParser.json());
+app.use(schedule)
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    console.log("Server is running on port " + port);
+})
